09a52cc2ba31077c4a4767127c7b1337c9e873ff
Alaricode <antonlarichev@gmail.com>
2025-05-07 12:55:31 +0300

    Доработка сборки

--- build.sh
+++ build.sh
@@ -3,6 +3,18 @@
 # Создаем директорию для сборки если её нет
 mkdir -p build
 
+# Определяем текущую ОС для правильной компиляции
+HOST_OS=$(go env GOOS)
+HOST_ARCH=$(go env GOARCH)
+
+# Загружаем зависимости
+echo "Загрузка зависимостей..."
+go mod tidy
+if [ $? -ne 0 ]; then
+    echo "Ошибка при загрузке зависимостей"
+    exit 1
+fi
+
 # Список платформ для сборки
 PLATFORMS=("windows/amd64" "windows/386" "darwin/amd64" "darwin/arm64" "linux/amd64" "linux/386")
 
@@ -23,8 +35,8 @@ do
     # Выводим информацию о текущей сборке
     echo "Сборка для $GOOS/$GOARCH"
     
-    # Запускаем сборку
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o "$output_name"
+    # Для всех платформ используем чистый Go с тегами для modernc.org/sqlite
+    env CGO_ENABLED=0 GOOS=$GOOS GOARCH=$GOARCH go build -tags "moderncsqlite,libsqlite3" -o "$output_name"
     
     if [ $? -ne 0 ]; then
         echo "Ошибка при сборке для $GOOS/$GOARCH"
@@ -32,4 +44,4 @@ do
     fi
 done
 
-echo "Сборка завершена успешно!" 
+echo "Сборка завершена успешно!"
--- go.mod
+++ go.mod
@@ -4,22 +4,24 @@ go 1.24.0
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.6
-	github.com/mattn/go-sqlite3 v1.14.24
+	github.com/golang-jwt/jwt/v5 v5.2.2
+	github.com/ncruces/go-sqlite3 v0.25.1
 	github.com/otiai10/opengraph/v2 v2.1.0
 )
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
-	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/ncruces/julianday v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/tetratelabs/wazero v1.9.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
 )
